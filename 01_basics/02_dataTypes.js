feaa054ea4eeb26470755a029def6d56c7580f60
Ritesh Kumar Singh <121288149+Ritesh140@users.noreply.github.com>
2024-07-09 19:26:37 +0000

    data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";  // treat all JS Code as newer version 
+// (3 + 3)  // not possible // we are using nodejs, not browser
+
+console.log(3+3)
+console.log("Ritesh")
+
+let name = "ritesh"
+let age = 24
+let isLoggedIn = false
+
+// some data types
+// number  => 2 to pweror 53
+// bigint   for larger number
+// string => ""
+// boolean => true/false
+// null => standalone value .// its type is object
+// undefined => type undefined
+
+// object 
+
+console.log(typeof "ritesh")
+console.log(typeof undefined);   // undefined
+console.log(typeof null);       // object
+console.log(typeof true)
+
+
